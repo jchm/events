9c76f42b50023b34520d73f9357961748b0febae
jchm <jochemschutte@gmail.com>
2015-09-04 23:16:34 +0200

    rewrite of event.js, is now a module, get events from Basis, other fixes

--- event.js
+++ event.js
@@ -1,79 +1,91 @@
-var days        = ['thursday','friday','saturday'];
-var dates       = [];
-var parties     = [];
+var days            = ['thursday','friday','saturday'];
+var dates           = [];
+var parties         = [];
+var eventsFiltered  = [];
 
-$(document).ready(function () {
+var dateFormat      = 'dddd';
 
-    var token = 'CAACEdEose0cBAI4WZBr4oGTHilYIHjUJat2ZAILlikpxYJaK4u0u4rZB9alrMBKpq0eKbKS2KYaKimQeIEJTEzRl9n69uGdsVYYr7XdrbFMI9eZCG5vaZBZBCybqA8Oj3uOZAlc0shFzpbDyBNhnVTEFOrK5mWQbYPvP3e4N61iqIXOyX9XVb45ZAmgwgWIlgsCzK9y91TvTpHyh9xnxXdj7';
+var FbToken = '1652860188281842|4lwtG-syhTXK-mYt9-DwvWXUnNw';
 
+(function(Event) {
 
-    $.ajaxSetup({ cache:true });
-    $.getScript('https://connect.facebook.net/en_US/sdk.js', function () {
+    Event.init = function () {
+        Event.setDates();
+        Event.setParties(dates);
 
-        FB.init({
-            appId: '1652860188281842',
-            version: 'v2.3'
-         });
+        Event.callFb();
+    };
 
-         var events          = [];
-         var events_params   = {
-            fields: 'name,start_time',
-            access_token: token,
-            since: '2015-06-22',
-            until: '2015-06-29'
-         };
+    Event.getResult = function () {
+        Event.init();
+        return parties;
+    };
 
-         events = FB.api('/205096312852338/events', events_params, function (response) {
-            console.log(response);
-            setDates();
-            filterEvents(response.data);
+    Event.callFb = function () {
+        $.ajaxSetup({ cache:true });
+        $.getScript('https://connect.facebook.net/en_US/sdk.js', function () {
 
-            console.log(parties);
-         });
-    });
- });
+             FB.init({
+                appId: '1652860188281842',
+                version: 'v2.3'
+             });
 
- function setDates() {
+             var events_params   = {
+                fields: 'name,start_time',
+                access_token: FbToken,
+                since: '2015-09-03',
+                until: '2015-09-06'
+             };
 
-    days.map( function( day ) {
-        dates.push( moment().day(day) );
-    });
+             FB.api('/205096312852338/events', events_params, function (response) {
+                Event.filterEvents(response.data);
+             });
 
- };
+             FB.api('/725935350828151/events', events_params, function (response) {
+                Event.filterEvents(response.data);
+             });
+        });
+    };
 
-function filterEvents(events) {
-    events.map( function( event ) {
+    Event.setDates = function () {
 
-         days.map( function( day ) {
+        days.map( function( day ) {
+            dates.push( moment().day(day) );
+        });
 
+    };
+
+    Event.setParties = function (days) {
+        days.map( function ( day ) {
             if ( parties[day] === undefined ) {
+                parties[day.format(dateFormat)] = [];
+            };
+        });
+    }
 
-                parties[day] = [];
+    Event.filterEvents = function (events) {
+        events.map( function( event ) {
 
+           var isParty = Event.checkEventTime(event);
+
+           if (isParty){
+                parties[moment(event.start_date).format(dateFormat)].push(event);
             };
 
-            if ( checkEvent( event, day ) ) {
-                parties[day].push( event );
-            };
-
-         });
-
-    });
-
-    console.log( parties );
- };
+        });
+     };
 
 
- function checkEvent(event, day) {
-        var friday      = moment().day( day ).hour(22).minute(59).second(59);
+     Event.checkEventTime = function (event) {
 
-        var is_on_day   = moment(event.start_time).isSame( friday , 'day');
-        var is_at_night = moment(event.start_time).isAfter( friday );
+        var isAtNight =  moment(event.start_time).isAfter(moment().hour(22),'hour');
 
-        if ( is_on_day && is_at_night ) {
-            console.log(event);
-            return event;
-        };
+        if (!isAtNight) {
+            return false;
+        }
 
+        return true;
+     };
 
- };
+})(window.Event = window.Event || {});
+
